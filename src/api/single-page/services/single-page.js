c8adaaa55a67ed6fb04d974ea215a1be832bc064
Rafael Alves <rafalves.costa@gmail.com>
2023-05-18 08:04:16 -0300

    added article scheme

--- src/api/single-page/services/single-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * single-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::single-page.single-page');
