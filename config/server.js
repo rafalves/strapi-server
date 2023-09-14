c457c64d1fa0fd78f4f26ecf0e64cc0b4c9e3326
Rafael Alves <rafalves.costa@gmail.com>
2023-09-14 00:23:32 -0300

    added faq component block

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: 'https://melhores-compras.online/dev',
+  url: 'https://melhores-compras.online/dev',
   app: {
     keys: env.array("APP_KEYS"),
   },
